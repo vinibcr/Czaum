d4f194f16e451436fc3c0d70ba0f2ec7424f30ba
Angelo Bonini <abonini2000@gmail.com>
2021-06-02 12:03:36 -0300

    some more random exercises from 2020

---
+++ lista1/excs24.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main(){
+int num;
+printf("digite o numero");
+scanf("%d", &num);
+printf("%d\n", num*1);
+printf("%d\n", num*2);
+printf("%d\n", num*3);
+printf("%d\n", num*4);
+printf("%d\n", num*5);
+printf("%d\n", num*6);
+printf("%d\n", num*7);
+printf("%d\n", num*8);
+printf("%d\n", num*9);
+printf("%d\n", num*10);
+printf("Acabou?\n");
+
+system("pause");
+}
+
+
+
+
+
