9067728b7e7c13fa2862b3d6e21d06d2fe2dafb6
Angelo Bonini <abonini2000@gmail.com>
2021-08-18 21:42:04 -0300

    adding work files

--- trabalho_MNC_2/mnc_t2.c
+++ trabalho_MNC_2/mnc_t2.c
@@ -3,78 +3,94 @@
 #define LIN_X 0
 #define LIN_Y 1
 
-//n: qte de subintervalos
-//a: limite inferior
-//b: limite superior
+//Lucas Augusto Ribeiro Massarico
+//RA: 191027723
 
-typedef struct {
+//Angelo Bonini
+//RA: 191026077
+
+typedef struct
+{
    double coef[MAX_GRAU_FUNCAO + 1]; //coeficiente do termo de grau i, variando de 0 a MAX_GRAU_FUNCAO
 } funcao;
 
-void iniciaFuncao(funcao *f, int c0, int c1, int c2, int c3) {
+void iniciaFuncao(funcao *f, int c0, int c1, int c2, int c3)
+{
    (*f).coef[0] = c0;
    (*f).coef[1] = c1;
    (*f).coef[2] = c2;
    (*f).coef[3] = c3;
 }
-double img(funcao f, double x) {
+double img(funcao f, double x)
+{
    double y = 0;
    for (int i = 0; i <= MAX_GRAU_FUNCAO; i++)
       y += f.coef[i] * pow(x, i);
    return y;
 }
 
-double trapezio_1ap(double y0, double y1, double h) {
+double trapezio_1ap(double y0, double y1, double h)
+{
    return (h * 1 / 2) * (y0 + y1);
 }
-double umTercoSimpson_1ap(double y0, double y1, double y2, double h) {
+double umTercoSimpson_1ap(double y0, double y1, double y2, double h)
+{
    return (h * 1 / 3) * (y0 + 4 * y1 + y2);
 }
-double tresOitavosSimpson_1ap(double y0, double y1, double y2, double y3, double h) {
+double tresOitavosSimpson_1ap(double y0, double y1, double y2, double y3, double h)
+{
    return (h * 3 / 8) * (y0 + 3 * y1 + 3 * y2 + y3);
 }
 
 //calcula o vetor y = {f(x), f(x+h), f(x+2h), ... , f(x+n*h)} de tamanho n+1
-void calculaY(int n, double h, double x, funcao f, double y[MAX]) {
+void calculaY(int n, double h, double x, funcao f, double y[MAX])
+{
    for (int i = 0; i <= n; i++)
       y[i] = img(f, x + i * h);
 }
 
-/* funcoes do t2 */
-void geraSubMatriz(int n, double a[][MAX], int c, double sub[][MAX]) {
+void geraSubMatriz(int n, double a[][MAX], int c, double sub[][MAX])
+{
    for (int i = 1; i < n; i++)
-      for (int j = 0, k = 0; j < n; j++, k++) {
+      for (int j = 0, k = 0; j < n; j++, k++)
+      {
          if (j != c)
             sub[i - 1][k] = a[i][j];
          else
             k--;
       }
 }
-double determinante(int n, double a[][MAX]) {
+double determinante(int n, double a[][MAX])
+{
    double s = 0;
    if (n == 1)
       return a[0][0];
-   else {
+   else
+   {
       double sub[MAX][MAX];
       for (int j = 0; j < n; j++)
-         if (a[0][j] != 0) { //para cada elemento da linha 0 (a[0][j])
+         if (a[0][j] != 0)
+         { //para cada elemento da linha 0 (a[0][j])
             geraSubMatriz(n, a, j, sub);
             s += a[0][j] * pow(-1, j) * determinante(n - 1, sub);
          }
    }
    return s;
 }
-bool temSubMatrizesNaoSingulares(int n, double a[][MAX]) {
+bool temSubMatrizesNaoSingulares(int n, double a[][MAX])
+{
    for (int i = 1; i <= n; i++)
       if (determinante(i, a) == 0)
          return false;
    return true;
 }
-bool sistemaTriangularSuperior(int n, double a[][MAX], double b[], double x[]) {
+bool sistemaTriangularSuperior(int n, double a[][MAX], double b[], double x[])
+{
    if (determinante(n, a) == 0)
       return false;
 
-   for (int i = n - 1; i >= 0; i--) {
+   for (int i = n - 1; i >= 0; i--)
+   {
       double s = 0;
       for (int j = i + 1; j < n; j++)
          s += a[i][j] * x[j];
@@ -83,11 +99,13 @@ bool sistemaTriangularSuperior(int n, double a[][MAX], double b[], double x[]) {
    }
    return true;
 }
-bool sistemaTriangularInferior(int n, double a[][MAX], double b[], double x[]) {
+bool sistemaTriangularInferior(int n, double a[][MAX], double b[], double x[])
+{
    if (determinante(n, a) == 0)
       return false;
 
-   for (int i = 0; i < n; i++) {
+   for (int i = 0; i < n; i++)
+   {
       double s = 0;
       for (int j = 0; j < i; j++)
          s += a[i][j] * x[j];
@@ -96,15 +114,18 @@ bool sistemaTriangularInferior(int n, double a[][MAX], double b[], double x[]) {
    }
    return true;
 }
-bool decomposicaoLU(int n, double a[][MAX], double b[], double x[]) {
+bool decomposicaoLU(int n, double a[][MAX], double b[], double x[])
+{
    double u[MAX][MAX], l[MAX][MAX], y[MAX], s;
 
    if (!temSubMatrizesNaoSingulares(n, a))
       return false; //método não converge
 
-   for (int p = 0; p < n; p++) {
+   for (int p = 0; p < n; p++)
+   {
       //calcula a linha p de U
-      for (int j = p; j < n; j++) {
+      for (int j = p; j < n; j++)
+      {
          s = 0;
          for (int k = 0; k < p; k++)
             s += l[p][k] * u[k][j];
@@ -112,7 +133,8 @@ bool decomposicaoLU(int n, double a[][MAX], double b[], double x[]) {
          u[p][j] = a[p][j] - s;
       }
       //calcula a coluna p de L
-      for (int i = p; i < n; i++) {
+      for (int i = p; i < n; i++)
+      {
          s = 0;
          for (int k = 0; k < p; k++)
             s += l[i][k] * u[k][p];
@@ -120,16 +142,14 @@ bool decomposicaoLU(int n, double a[][MAX], double b[], double x[]) {
          l[i][p] = (a[i][p] - s) / u[p][p];
       }
    }
-   // impMatriz(n, l);
-   // impMatriz(n, u);
    sistemaTriangularInferior(n, l, b, y); //L.y = b
    sistemaTriangularSuperior(n, u, y, x); //U.x = y
    return true;
 }
 
-/* --- */
 
-int fat(int n) {
+int fat(int n)
+{
    int fat = 1;
    for (int i = 2; i <= n; i++)
       fat *= i;
@@ -137,7 +157,8 @@ int fat(int n) {
 }
 
 //retorna Pn(x), sendo Pn o polinômio de grau n-1
-double newton(double x, int n, double tabela[][MAX]) {
+double newton(double x, int n, double tabela[][MAX])
+{
    //difDiv[i][j]: j-ésima diferença dividida de Yi
    double difDiv[MAX][MAX];
 
@@ -150,7 +171,8 @@ double newton(double x, int n, double tabela[][MAX]) {
 
    //cálcula o valor do polinômio interpolador em x
    double res = 0, tmp;
-   for (int j = 0; j < n; j++) {
+   for (int j = 0; j < n; j++)
+   {
       tmp = difDiv[0][j];
       for (int k = 0; k < j; k++)
          tmp *= x - tabela[LIN_X][k];
@@ -159,7 +181,8 @@ double newton(double x, int n, double tabela[][MAX]) {
    return res;
 }
 //pressupõe-se que os valores de x sejam equidistantes e ordenados
-double newtonGregory(double x, int n, double tabela[][MAX]) {
+double newtonGregory(double x, int n, double tabela[][MAX])
+{
    //difFin[i][j]: j-ésima diferença finita de Yi
    double difFin[MAX][MAX];
 
@@ -172,7 +195,8 @@ double newtonGregory(double x, int n, double tabela[][MAX]) {
 
    //cálcula o valor do polinômio interpolador em x
    double res = 0, h = tabela[LIN_X][1] - tabela[LIN_X][0], tmp;
-   for (int j = 0; j < n; j++) {
+   for (int j = 0; j < n; j++)
+   {
       tmp = difFin[0][j] / (fat(j) * pow(h, j));
       for (int k = 0; k < j; k++)
          tmp *= x - tabela[LIN_X][k];
@@ -181,10 +205,12 @@ double newtonGregory(double x, int n, double tabela[][MAX]) {
    return res;
 }
 
-double coefDeterminacao(int n, double tabela[][MAX], double yAjustado[]) {
+double coefDeterminacao(int n, double tabela[][MAX], double yAjustado[])
+{
    double erro2 = 0, y2 = 0, y = 0; //somatórios
 
-   for (int i = 0; i < n; i++) {
+   for (int i = 0; i < n; i++)
+   {
       erro2 += pow(tabela[LIN_Y][i] - yAjustado[i], 2);
       y += tabela[LIN_Y][i];
       y2 += pow(tabela[LIN_Y][i], 2);
@@ -193,10 +219,12 @@ double coefDeterminacao(int n, double tabela[][MAX], double yAjustado[]) {
 }
 
 //ajusta os pontos a uma reta do tipo y = a0 + a1.x
-void ajusteReta(int n, double tabela[][MAX], double *a0, double *a1, double yAjustado[], double *coefDet) {
+void ajusteReta(int n, double tabela[][MAX], double *a0, double *a1, double yAjustado[], double *coefDet)
+{
    double xy = 0, x = 0, y = 0, x2 = 0; //somatórios
 
-   for (int j = 0; j < n; j++) {
+   for (int j = 0; j < n; j++)
+   {
       xy += tabela[LIN_X][j] * tabela[LIN_Y][j];
       x += tabela[LIN_X][j];
       y += tabela[LIN_Y][j];
@@ -212,20 +240,22 @@ void ajusteReta(int n, double tabela[][MAX], double *a0, double *a1, double yAju
 }
 
 //ajusta os pontos a um polinomio de grau m do tipo y = a[0] + a[1].x + a[2].x^2 + ... + a[m].x^m
-void ajustePolinomial(int m, int n, double tabela[][MAX], double a[], double yAjustado[], double *coefDet) {
+void ajustePolinomial(int m, int n, double tabela[][MAX], double a[], double yAjustado[], double *coefDet)
+{
    double coef[MAX][MAX], termInd[MAX];
 
    //calcula matriz dos coeficientes do sistema de ordem m+1
    for (int i = 0; i <= m; i++)
-      for (int j = 0; j <= m; j++) {
+      for (int j = 0; j <= m; j++)
+      {
          coef[i][j] = 0;
          for (int p = 0; p < n; p++) //se i+j = 0, coef[i][j] = n
             coef[i][j] += pow(tabela[LIN_X][p], i + j);
       }
-   // impMatriz(m+1, coef);
 
    //calcula termos independentes do sistema de ordem m+1
-   for (int i = 0; i <= m; i++) {
+   for (int i = 0; i <= m; i++)
+   {
       termInd[i] = 0;
       for (int p = 0; p < n; p++) //se i = 0, termInd[i] = somatorio de y
          termInd[i] += tabela[LIN_Y][p] * pow(tabela[LIN_X][p], i);
@@ -233,7 +263,8 @@ void ajustePolinomial(int m, int n, double tabela[][MAX], double a[], double yAj
    // impVetor(m+1, termInd);
    decomposicaoLU(m + 1, coef, termInd, a);
 
-   for (int i = 0; i < n; i++) {
+   for (int i = 0; i < n; i++)
+   {
       yAjustado[i] = 0;
       for (int k = 0; k <= m; k++)
          yAjustado[i] += a[k] * pow(tabela[LIN_X][i], k);
@@ -242,7 +273,8 @@ void ajustePolinomial(int m, int n, double tabela[][MAX], double a[], double yAj
 }
 
 //ajusta os pontos a uma curva do tipo y = a.(b^x)
-void ajusteExponencial(int n, double tabela[][MAX], double *a, double *b, double yAjustado[], double *coefDet) {
+void ajusteExponencial(int n, double tabela[][MAX], double *a, double *b, double yAjustado[], double *coefDet)
+{
    double a0, a1;
    for (int j = 0; j < n; j++)
       tabela[LIN_Y][j] = log(tabela[LIN_Y][j]);
@@ -253,7 +285,8 @@ void ajusteExponencial(int n, double tabela[][MAX], double *a, double *b, double
    *b = pow(M_E, a1);
 }
 
-double trapezio(int n, double a, double b, funcao f) {
+double trapezio(int n, double a, double b, funcao f)
+{
    double res = 0, y[MAX];
    double h = (b - a) / n;
 
@@ -266,7 +299,8 @@ double trapezio(int n, double a, double b, funcao f) {
    return res;
 }
 
-double umTercoSimpson(int n, double a, double b, funcao f) {
+double umTercoSimpson(int n, double a, double b, funcao f)
+{
    double res = 0, y[MAX];
    double h = (b - a) / n;
 
@@ -282,7 +316,8 @@ double umTercoSimpson(int n, double a, double b, funcao f) {
    return res;
 }
 
-double tresOitavosSimpson(int n, double a, double b, funcao f) {
+double tresOitavosSimpson(int n, double a, double b, funcao f)
+{
    double res = 0, y[MAX];
    double h = (b - a) / n;
 
@@ -292,7 +327,8 @@ double tresOitavosSimpson(int n, double a, double b, funcao f) {
    for (int i = 0; i + 3 <= n; i += 3)
       res += tresOitavosSimpson_1ap(y[i], y[i + 1], y[i + 2], y[i + 3], h);
 
-   if (n % 3 != 0) {
+   if (n % 3 != 0)
+   {
       if (n % 2 == 0)
          res += umTercoSimpson_1ap(y[n - 2], y[n - 1], y[n], h); // faltaram 2 intervalos
       else
@@ -302,8 +338,8 @@ double tresOitavosSimpson(int n, double a, double b, funcao f) {
    return res;
 }
 
-
-int menu() {
+int menu()
+{
    int op;
    printf("   Interpolação Polinomial  \n");
    printf("         Ajuste de Curvas e Integração Numérica        \n");
@@ -317,7 +353,8 @@ int menu() {
    printf("     07 › Rotina Trapézio\n\n");
    printf("     08 › Um Terço de Simpson\n");
    printf("     09 › TRes Oitvaos de Simpson\n\n");
-   do {
+   do
+   {
       printf("Resposta.: ");
       scanf("%d", &op);
    } while (op < 1 || op > 10);
@@ -325,14 +362,18 @@ int menu() {
    return op;
 }
 
-int main() {
-    funcao f1, f2, f3;
-  int op, n, maxIte, ite;
-   double a[MAX][MAX], i[MAX][MAX], x[MAX], x0[MAX], b[MAX], e;
+int main()
+{
+   funcao f1, f2, f3;
+   int op, n, ;
+   double a[MAX][MAX], tabela[][MAX], a[],;
+   double x, y[];
+   double l, f, h;
    char r;
    bool ok;
 
-   do {
+   do
+   {
       system("clear");
       op = menu();
 
@@ -341,67 +382,42 @@ int main() {
       printf("\nCoeficientes da matriz %dx%d:\n", n, n);
       leMatriz(n, a);
 
-      if (op == 1)
-         printf("\nA solução é: %.4lf\n", determinante(n, a));
-      else if (op >= 2 && op <= 7) {
-         printf("\nTermos independentes:\n");
-         leVetor(n, b);
+      switch (op)
+      {
+      case 1:
+         ok = newton(x, n, b, tabela);
+         break;
+      case 2:
+         ok = newtonGregory(x, n, tabela);
+         break;
+      case 3:
+         ok = coefDeterminacao(n, tabela, y);
+         break;
+      case 4:
+         ok = ajusteReta(n, tabela, *a0, *a1, y, *coefDet);
+         break;
+      case 5:
+         ok = ajustePolinomial(m,n, tabela, a, y, *coefDet);
+         break;
+      case 6:
+         ok = ajusteExponencial(n, tabela, *a, *b, y[], *coefDet);
+         break;
+      case 7:
+         ok = trapezio(n,l , f, f1);
+         break;
+           case 8:
+         ok = umTercoSimpson(n, l, f, f2);
+         break;
+           case 9:
+         ok = tresOitavosSimpson(n, l, f, h, f3);
+         break;
 
-         switch (op) {
-         case 2:
-            ok = sistemaTriangularInferior(n, a, b, x);
-            break;
-         case 3:
-            ok = sistemaTriangularSuperior(n, a, b, x);
-            break;
-         case 4:
-            ok = decomposicaoLU(n, a, b, x);
-            break;
-         case 5:
-            ok = cholesky(n, a, b, x);
-            break;
-         case 6:
-            ok = gaussCompacto(n, a, b, x);
-            break;
-         case 7:
-            ok = gaussJordan(n, a, b, x);
-            break;
-         }
-         if (ok) {
-            printf("\nO vetor solução é:\n");
-            impVetor(n, x);
-         } else
-            printf("\nO método não converge!\n");
-      } else if (op == 8 || op == 9) {
-         printf("\nTermos independentes:\n");
-         leVetor(n, b);
-         printf("\nAproximação inicial para o vetor solução:\n");
-         leVetor(n, x0);
-         printf("\nErro: ");
-         scanf("%lf", &e);
-         printf("\nNúmero máximo de iterações: ");
-         scanf("%d", &maxIte);
-
-         if (op == 8)
-            ok = jacobi(n, a, b, e, x0, maxIte, x, &ite);
-         else
-            ok = gaussSeidel(n, a, b, e, x0, maxIte, x, &ite);
-
-         if (ok) {
-            printf("\nO vetor solução é:\n");
-            impVetor(n, x);
-         } else
-            printf("\nO método não converge!\n");
-      } else {
-         ok = matrizInversa(n, a, i);
-         if (ok) {
-            printf("A matriz solução é:\n");
-            impMatriz(n, i);
-         } else
-            printf("\nO método não converge!\n");
       }
 
-      do {
+
+
+      do
+      {
          printf("\nRetornar ao menu [s/n]? ");
          fflush(stdin);
          scanf(" %c", &r);
